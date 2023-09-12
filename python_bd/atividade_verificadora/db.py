4d8381f919d86c6c982b0c62ec266aa07d99ebc5
Vinicius Hansel <92490812+HanselVinicius@users.noreply.github.com>
2023-09-11 22:04:48 -0300

    Update db.py

--- python_bd/atividade_verificadora/db.py
+++ python_bd/atividade_verificadora/db.py
@@ -4,16 +4,25 @@ class DbUtils:
     pass
 
 
-    def criarTabelas():
+      def criarTabelas():
         conexao = conector.connect('escola.db')
         cursor = conexao.cursor()
         #tabela alunos
-        sql = "create table if not exists Aluno(matricula integer primary key AUTOINCREMENT, nome text,curso text);"
+        sql = '''CREATE TABLE if not exists tbaluno (
+            matricula INTEGER NOT NULL,
+            nome TEXT NOT NULL,
+            curso TEXT NOT NULL,
+            PRIMARY KEY (matricula)
+            );'''
         cursor.execute(sql)
         #tabela nota
-        sql = "create table if not exists Nota(item integer primary key AUTOINCREMENT, valor real)"
+        sql = '''CREATE TABLE if not exists tbnota (
+            item INTEGER PRIMARY KEY AUTOINCREMENT,
+            valor FLOAT NOT NULL,
+            matricula INTEGER NOT NULL,
+            FOREIGN KEY (matricula) REFERENCES tbaluno(matricula)
+            );'''
         conexao.commit()
         cursor.close()
         conexao.close()
-
 
