4db110b0668de13cb2106a894d146537b5e4ce99
HanselVinicius <hanelvinicius@gmail.com>
2023-09-19 21:23:25 -0300

    aulas 4 5 6

--- aula_3/arquivo.py
+++
@@ -1,9 +0,0 @@
-def crescente():
-        arquivo = open("crescente.txt",'w')
-        numero = 0
-        while numero != 101:
-            arquivo.write(str(numero)+"; \n")
-            numero = numero+1
-        arquivo.close()
-
-crescente()
